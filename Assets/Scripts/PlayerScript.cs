414029f19cee258192ecf8907f35f8d46fd6de19
QLevaslot <quentin.levaslot@gmail.com>
2014-08-21 00:01:07 +0200

    Added jump basis

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
 
 public class PlayerScript : MonoBehaviour {
 
@@ -9,24 +9,44 @@ public class PlayerScript : MonoBehaviour {
 	// Store the movement
 	private Vector2 movement;
 
+	private float jumpTime = 0;
+
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log ("Player initialisation");
 	}
 
 	void Update(){
 		// Retrieve axis information
 		float inputX = Input.GetAxis("Horizontal");
 		float inputY = Input.GetAxis("Vertical");
-		
+
+		// Trying to re-use that input for now
+		bool shoot = Input.GetButtonDown("Fire1");
+		shoot |= Input.GetButtonDown("Fire2");
+
+		if (shoot) {
+			jumpTime = 1f;		
+		}
+
+		float jump = 0;
+		if (jumpTime > 0f) {
+			jump = 1;		
+		}
 		movement = new Vector2(
 			speed.x * inputX,
-			speed.y * inputY);
+			speed.y * jump);
 
 	}
+
 
 	void FixedUpdate(){
 		// Move the game object
 		rigidbody2D.velocity = movement;
+
+		// Jumptime decrease
+		if (jumpTime > 0) {
+			jumpTime = jumpTime - 0.1f;		
+		}
 	}
 }
